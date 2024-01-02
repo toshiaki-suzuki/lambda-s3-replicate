a371b77ded380e27878e516d54dce4a2e8492c08
toshiaki-suzuki <saki0x4bf@gmail.com>
2024-01-03 00:29:26 +0900

    Update bucket names in create_buckets.sh script

--- create_buckets.sh
+++ create_buckets.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 echo "Creating buckets..."
-aws --endpoint-url=http://localstack:4566 s3api create-bucket --bucket 送信元バケット名 # 送信元バケット名を書き換える
-aws --endpoint-url=http://localstack:4566 s3api create-bucket --bucket 送信先バケット名 # 送信先バケット名を書き換える
+aws --endpoint-url=http://localstack:4566 s3api create-bucket --bucket source-bucket
+aws --endpoint-url=http://localstack:4566 s3api create-bucket --bucket destination-bucket
 echo "Created buckets!"
